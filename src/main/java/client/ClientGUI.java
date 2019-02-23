d4b0cc29c95ba1a00f5dd06087ca5b29597a8e26
Unknown <m.karsten10@gmail.com>
2019-02-23 14:42:11 +0100

    added fxml

--- src/main/java/client/ClientGUI.java
+++ src/main/java/client/ClientGUI.java
@@ -1,15 +1,40 @@
 package client;
 
-import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class ClientGUI extends Application{
+import java.io.IOException;
+
+public class ClientGUI extends Application {
 
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
+        try {
 
+            Parent root = FXMLLoader.load(getClass().getResource("/client.fxml"));
+        } catch (IOException e) {
 
+            System.out.println("There was an issue loading the fxml.");
+        }
+
+        String screenText = "Save the Enviroment!";
+
+        Label test = new Label("Hello, JavaFX " + screenText);
+
+        Scene mainScene = new Scene(new StackPane(test), 640,480);
+        stage.setScene(mainScene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
     }
 }

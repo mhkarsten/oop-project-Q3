7b0bf3be7da2950911c07894976ee59bd50f7dbc
harshitaa <harshi.pandey14@gmail.com>
2019-03-01 14:59:52 +0100

    Added my new files of GUI, images,main and root.

---
+++ src/main/java/main/SidebarHoverEffect.java
@@ -0,0 +1,28 @@
+package main;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.io.File;
+
+public class SidebarHoverEffect extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("src/root/FXMLRoot.fxml"));
+        URL url = new File("src/root/FXMLRoot.fxml").toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root, 400, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}

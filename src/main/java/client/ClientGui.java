ce93c2f59e3bb0a5bd6836e70ce4f1a1e6206876
harshitaa <harshi.pandey14@gmail.com>
2019-03-06 12:11:53 +0100

    New changes to main

--- src/main/java/client/ClientGui.java
+++ src/main/java/client/ClientGui.java
@@ -14,37 +14,19 @@ public class ClientGui extends Application {
 
 
     @Override
-<<<<<<< HEAD:src/main/java/client/ClientGUI.java
+
     public void start(Stage stage) throws IOException {
 
-        URL url = new File("src/main/java/resources/client.fxml").toURL();
+        URL url = new File("src/main/resources/loginScreen.fxml").toURL();
         Parent root = FXMLLoader.load(url);
         String screenText = "Save the Enviroment!";
 
-        Label test = new Label(screenText);
+        Scene scene = new Scene(root, 600,400);
 
-        Scene mainScene = new Scene(new StackPane(test), 640,480);
-        stage.setScene(mainScene);
+
+        stage.setScene(scene);
         stage.show();
-=======
-    public void start(Stage stage) {
-
-        try {
-            URL resourceUrl = getClass().getResource("src/main/resources/loginScreen.fxml");
-            Parent root = FXMLLoader.load(resourceUrl);
-
-            Scene scene = new Scene(root, 600, 400);
-
-            stage.setScene(scene);
-            stage.show();
-
-        } catch (IOException e) {
-            System.out.println("There was an issue loading the fxml.");
-            e.printStackTrace();
-        }
->>>>>>> 4d16df88e15aeb88869fa0c5574e9d295d9c3cb8:src/main/java/client/ClientGui.java
     }
-
     public static void main(String[] args) {
 
         launch(args);

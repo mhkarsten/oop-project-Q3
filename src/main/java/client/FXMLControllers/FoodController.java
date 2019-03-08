b6d8155d65303f03b87d88f9ca913b1aa04f6388
Unknown <m.karsten10@gmail.com>
2019-03-08 16:20:55 +0100

    Meal selection

--- src/main/java/client/FXMLControllers/FoodController.java
+++ src/main/java/client/FXMLControllers/FoodController.java
@@ -1,105 +1,109 @@
 package client.FXMLControllers;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.IntegerBinding;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableSet;
+import client.foodAPI.Meal;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.shape.Circle;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
-import static client.foodAPI.FoodAPI.getRandomMeal;
+import static client.foodAPI.FoodAPI.*;
 
-public class FoodController {
-
-    @FXML
-    private Label meal1;
-    @FXML
-    private Label meal2;
-    @FXML
-    private Label meal3;
-    @FXML
-    private Label meal4;
-
+public class FoodController implements Initializable {
 
     @FXML
-    private Circle selectCircle1;
-    @FXML
-    private Circle selectCircle2;
-    @FXML
-    private Circle selectCircle3;
-    @FXML
-    private Circle selectCircle4;
+    public Label meal1;
+    public Label meal2;
+    public Label meal3;
+    public Label meal4;
 
-    @FXML
-    private CheckBox veganOpt;
-    @FXML
-    private CheckBox vegOpt;
-    @FXML
-    private CheckBox meatOpt;
+    public Circle selectCircle1;
+    public Circle selectCircle2;
+    public Circle selectCircle3;
+    public Circle selectCircle4;
 
-    @FXML
-    private Button mealSelected;
+    public CheckBox veganOpt;
+    public CheckBox vegOpt;
+    public CheckBox meatOpt;
 
-    public void selectMeal(MouseEvent event) {
+    public Button mealSelected;
 
-        event.getSource();
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
 
+        meal1.setText(getRandomMeal()[0].getStrMeal());
+        meal2.setText(getRandomMeal()[0].getStrMeal());
+        meal3.setText(getRandomMeal()[0].getStrMeal());
+        meal4.setText(getRandomMeal()[0].getStrMeal());
     }
 
-    private ObservableSet<CheckBox> selectedBoxes = FXCollections.observableSet();
-    private ObservableSet<CheckBox> unselectedBoxes = FXCollections.observableSet();
 
-    private IntegerBinding numBoxesSelected = Bindings.size(selectedBoxes);
+    public void setMealStrings(ArrayList<Meal[]> mealCategory, int offset) {
 
-    private final int maxNumSelected =  1;
+        HashMap<String, Label> varNameMap = new HashMap<>();
+        varNameMap.put("meal1", meal1);
+        varNameMap.put("meal2", meal2);
+        varNameMap.put("meal3", meal3);
+        varNameMap.put("meal4", meal4);
 
+        if (mealCategory.size() < 4) {
 
+            int offsetAddition = 0;
 
-//    @Override
-//    public void initialize(URL url, ResourceBundle rb) {
-//
-//        meal1.setText(getRandomMeal()[0].getStrMeal());
-//        meal2.setText(getRandomMeal()[0].getStrMeal());
-//        meal3.setText(getRandomMeal()[0].getStrMeal());
-//        meal4.setText(getRandomMeal()[0].getStrMeal());
-//
-//    }
+            for (int i = 1; i < mealCategory.size() + 1; i++) {
 
-    public void configureMealBoxes(CheckBox box) {
+                varNameMap.get("meal" + i).setText(mealCategory.get(offset + offsetAddition)[0].getStrMeal());
 
-        if(box.isSelected()) {
+                offsetAddition++;
+            }
 
-            selectedBoxes.add(box);
+            for (int i = mealCategory.size() + 1; i <= 4; i++) {
+
+                varNameMap.get("meal" + i).setText("");
+            }
         } else {
 
-            unselectedBoxes.add(box);
+            meal1.setText(mealCategory.get(offset)[0].getStrMeal());
+            meal2.setText(mealCategory.get(offset + 1)[0].getStrMeal());
+            meal3.setText(mealCategory.get(offset + 2)[0].getStrMeal());
+            meal4.setText(mealCategory.get(offset + 3)[0].getStrMeal());
         }
-
-        box.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
-            if (isNowSelected) {
-
-                unselectedBoxes.remove(box);
-                selectedBoxes.add(box);
-            } else {
-
-                selectedBoxes.remove(box);
-                unselectedBoxes.add(box);
-            }
-        });
     }
 
-    public void changeMeals(ActionEvent event) {
+    public void displayVegMeals() {
 
-        event.getID();
+        meatOpt.setSelected(false);
+        veganOpt.setSelected(false);
 
+        ArrayList<Meal[]> vegetarianMeals = getMealCategory("Vegetarian");
 
+        setMealStrings(vegetarianMeals, 0);
     }
+
+    public void displayVeganMeals() {
+
+        meatOpt.setSelected(false);
+        vegOpt.setSelected(false);
+
+        ArrayList<Meal[]> veganMeals = getMealCategory("Vegan");
+
+        setMealStrings(veganMeals, 0);
+    }
+
+    public void displayMeatMeals() {
+
+        vegOpt.setSelected(false);
+        veganOpt.setSelected(false);
+
+        ArrayList<Meal[]> meatMeals = getAllMeatMeals();
+
+        setMealStrings(meatMeals, 0);
+    }
+    
 }

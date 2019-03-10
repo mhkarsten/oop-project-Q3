7ca6d0f03ea9ee72c21d5e0f60938d96f8970b23
Unknown <m.karsten10@gmail.com>
2019-03-10 19:27:05 +0100

    Veg Meal update
    currently Jennifer with userID 3 is hard coded into the system. However, if you select a vegetarian meal, it will send requests the the server

--- src/main/java/client/FXMLControllers/ProfileController.java
+++ src/main/java/client/FXMLControllers/ProfileController.java
@@ -1,30 +1,27 @@
 package client.FXMLControllers;
-import client.serverCommunication.User;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ProfileController implements Initializable {
+import static server.controller.ServerController.getUser;
 
-    private User activeUser;
+public class ProfileController implements Initializable {
 
     @FXML
     public Label userName;
+    public Label userID;
+    public Label userPoints;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+        server.model.User currentUser = getUser("3");
 
-
+        userName.setText("User Name: " + currentUser.getUserName());
+        userID.setText("User ID: " + currentUser.getUserID());
+        userPoints.setText("User Points: " + currentUser.getUserPoints());
     }
-
-    public void getActiveUser(int userID) {
-
-
-    }
-
-
 }
--- src/main/java/client/FXMLControllers/ScoreController.java
+++ src/main/java/client/FXMLControllers/ScoreController.java
@@ -1,12 +1,26 @@
 package client.FXMLControllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import server.model.User;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static server.controller.ServerController.getUser;
 
 
-public class ScoreController {
+public class ScoreController implements Initializable {
 
     @FXML
     public Label userPoints;
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        User currentUser = getUser("3");
+
+        userPoints.setText("Points: " + currentUser.getUserPoints());
+    }
 }

4c3eb84aa4b9ed5749baade4b04fdac797a23da7
Unknown <m.karsten10@gmail.com>
2019-03-31 17:32:43 +0200

    Compare Page FXML

--- src/main/java/client/UI/CompareController.java
+++ src/main/java/client/UI/CompareController.java
@@ -1,4 +1,53 @@
 package client.UI;
 
-public class CompareController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CompareController implements Initializable {
+
+    @FXML
+    Button followBtn;
+
+    @FXML
+    Label userName;
+    @FXML
+    Label compareName;
+    @FXML
+    Label userPoints;
+    @FXML
+    Label comparePoints;
+    @FXML
+    Label findStatus;
+
+    @FXML
+    TextField userInput;
+
+    @FXML
+    ListView userFollowing;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        UserSession.getInstace().getUser.getId();
+
+    }
+
+    public void findUserFollow() {
+
+
+    }
+
+
+
+
+
+
 }

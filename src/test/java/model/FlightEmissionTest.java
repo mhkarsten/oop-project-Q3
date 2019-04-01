97e5333074b969137dfae8d697e62f2c307d7059
Dan Dan Berendsen <D.D.J.Z.Berendsen@student.tudelft.nl>
2019-04-01 12:24:58 +0200

    Added Test and cleaned some files a bit

--- src/test/java/model/FlightEmissionTest.java
+++ src/test/java/model/FlightEmissionTest.java
@@ -1,10 +1,21 @@
 package model;
 
 import org.junit.jupiter.api.Test;
+import server.model.FlightEmission;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlightEmissionTest {
+
+    String carbon = "333.3";
+    String carbontest = "823";
+    String energy = "3";
+    String energytest = "9";
+    String fuel_use = "7";
+    String fuel_usetest = "837";
+    String distance = "847";
+    String distancetest = "666";
+    FlightEmission fm = new FlightEmission(carbon, energy, fuel_use, distance);
 
     @Test
     void JSONtoFlight() {
@@ -12,33 +23,45 @@ class FlightEmissionTest {
 
     @Test
     void getCarbon() {
+        assertEquals(carbon, fm.getCarbon());
     }
 
     @Test
     void setCarbon() {
+        fm.setCarbon(carbontest);
+        assertEquals(carbontest, fm.getCarbon());
     }
 
     @Test
     void getEnergy() {
+        assertEquals(energy, fm.getEnergy());
     }
 
     @Test
     void setEnergy() {
-    }
-
-    @Test
-    void getDistance() {
-    }
-
-    @Test
-    void setDistance() {
+        fm.setEnergy(energytest);
+        assertEquals(energytest, fm.getEnergy());
     }
 
     @Test
     void getFuelUse() {
+        assertEquals(fuel_use, fm.getFuelUse());
     }
 
     @Test
     void setFuelUse() {
+        fm.setFuelUse(fuel_usetest);
+        assertEquals(fuel_usetest, fm.getFuelUse());
+    }
+
+    @Test
+    void getDistance() {
+        assertEquals(distance, fm.getDistance());
+    }
+
+    @Test
+    void setDistance() {
+        fm.setDistance(distancetest);
+        assertEquals(distancetest, fm.getDistance());
     }
 }
